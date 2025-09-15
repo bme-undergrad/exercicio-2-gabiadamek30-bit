02545730835b0da43361eb8c3a102d767341deb0
MARCO AURELIO DA CRUZ MENEGHETTI <MARCOMENEGHETTI@MARCOs-MacBook-Pro.local>
2025-09-15 13:54:48 -0300

    exercicio 2 tentativa 1

--- exercicio2.m
+++ exercicio2.m
@@ -1,4 +1,4 @@
-function [blendA, blendB] = exercicio2(NG)
+function [blendA, blendB] = exercicio2(3)
 
 % NG: numero do grupo
 
@@ -10,10 +10,22 @@ pkg load optim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
+f = [10,14];
 
+x0 = [1,1];
 
-% mantenha essas duas linhas finais
+A = [-0.3,-0.2;
+    -0.2,-0.25;
+    -0.25,-0.3;
+    1;
+    0.1];
+
+b = [-15;-10;-12;20;50];
+
+x_min_valores = zeros(2,1);
+x_max_valores = 1000 * ones(2,1);
+x = linprog(f,A,b);
+
 blendA = x(1);
 blendB = x(2);
 
